507034197905b2b351f5156d3784277cb56af15e
norrisanator <matthew.norris96@gmail.com>
2016-04-21 17:35:23 +1200

    Update AVC_robot.c
    Set up some example code for taking picture and getting the "whiteness" of each pixel. 
    Prints every half a second

--- AVC_robot.c
+++ AVC_robot.c
@@ -6,11 +6,20 @@ extern "C" int Sleep( int sec, int usec );
 // motor is the pin it is connected to, dir is direction 1 or 2, speed is 0-255
 extern "C" int SetMotor(int motor, int dir, int speed);
 
-int main()
-{
+int main(){
   // Sets up raspbery pi hardware and ensures everything is working.
   InitHardware();
-  
+//test code for camera, takes picture and prints it.
+  while(true){
+    // reads current image from camera stores in memory.
+    int take_picture();
+    // returns "whiteness" of each pixel.
+    char get_pixel(160,120,3);
+    printf("%d\n",get_pixel);
+    //repeats every half second.
+    Sleep(0.500000);
+  }
+
   
   return 0;
 }
