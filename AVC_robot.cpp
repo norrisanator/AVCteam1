7a05116c0668cd4514cb701d70525cacaedfbb2b
norrisanator <matthew.norris96@gmail.com>
2016-05-05 11:20:30 +1200

    Cleaned up code
    added in some network code.

--- AVC_robot.cpp
+++ AVC_robot.cpp
@@ -1,5 +1,7 @@
 #include  <stdio.h>
 #include  <time.h>
+// constants
+const int PICTURE_WIDTH = 320;
 
 extern "C" int init(int d_lev);
 extern "C" int take_picture();
@@ -8,34 +10,37 @@ extern "C" int Sleep( int sec, int usec );
 // Motor is the pin it is connected to, dir is direction 1 or 2, speed is 0-255
 extern "C" int set_motor(int motor, int speed);
 
-const int PICTURE_WIDTH = 320;
+extern "C" int connect_to_server( char server_addr[15],int port);
+extern "C" int send_to_server(char message[24]);
+extern "C" int receive_from_server(char message[24]);
+
+
+
 
 int main(){
   // Sets up raspbery pi hardware and ensures everything is working.
-    init(0)
-	// Test turning motors on for 5 seconds
-	set_motor(1, 255);
+    init(0);
+    // Test turning motors on for 5 seconds
+    set_motor(1, 255);
     set_motor(2, -255);
     Sleep(5,0);
     set_motor(1, 0);
     set_motor(2, 0);
-  // Test code for camera, takes picture and prints it.
-  char c;
-  while(true){
-    // Reads current image from camera stores in memory.
-    take_picture();
-	int total=0;
-	char c;
-	for(int i=0; i<PICTURE_WIDTH; i++){
-		c = get_pixel(120, i-(PICTURE_WIDTH/2), 3);
-		total += i*c;
-	}
+    // Test code for camera, takes picture and prints it.
+    char c;
+    while(true){
+        // Reads current image from camera stores in memory.
+        take_picture();
+    	int total=0;
+    	char c;
+    	for(int i=0; i<PICTURE_WIDTH; i++){
+	    	c = get_pixel(120, i-(PICTURE_WIDTH/2), 3);
+	    	total += i*c;
+	    }
 	
-    printf("%d\n",total);
-    // Repeats every half second.
-    Sleep(0,500000);
-  }
-
-  
-  return 0;
+        printf("%d\n",total);
+        // Repeats every half second.
+        Sleep(0,500000);
+    }
+    return 0;
 }
